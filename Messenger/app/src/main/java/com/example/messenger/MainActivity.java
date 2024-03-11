ac51e3bd540d7fdcc9d62cef0306208d67fa0437
DMaleesha <maleesha.live123@gmail.com>
2024-03-11 12:29:01 +0530

    Updated onSendMessage method with createChooser()

--- Messenger/app/src/main/java/com/example/messenger/MainActivity.java
+++ Messenger/app/src/main/java/com/example/messenger/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // call onSendMessage function when button is clicked
-    public void onSendMessage(View view){
+/*    public void onSendMessage(View view){
 
         // get text from the EditText
         EditText messageView = (EditText) findViewById(R.id.message);
@@ -28,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
         // add the text to the intent
         intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
+    } */
+
+    public void onSendMessage(View view) {
+        // get text from the EditText
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        // Create an Intent with the action ACTION_SEND
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        // Set the type of data to be sent
+        intent.setType("text/plain");
+
+        // Attach the message text to the intent using EXTRA_TEXT
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+
+        // get the chooser title
+        String chooserTitle = getString(R.string.chooser);
+
+        // Display the chooser dialog
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+
+        // Start the activity (displays the chooser dialog)
+        startActivity(chosenIntent);
     }
+
 }
